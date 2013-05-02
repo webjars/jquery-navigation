6fe479eac09ecf52c57f23f4d12b218822ad02e6
Christopher Hunt <huntchr@gmail.com>
2013-05-02 18:50:35 +1000

    Verified the name

--- src/main/resources/webjars-requirejs.js
+++ src/main/resources/webjars-requirejs.js
@@ -3,6 +3,6 @@
 // Ensure any request for this webjar brings in jQuery.
 requirejs.config({
     shim: {
-        "jquery.navigation": [ 'webjars!jquery.js' ]
+        "nav": [ 'webjars!jquery.js' ]
     }
 });
